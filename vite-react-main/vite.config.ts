55fabdef1c306ace48220818facc211cd5c7322d
atsushihigashie-code <atsushihigashie@gmail.com>
2026-07-11 15:20:52 +0900

    Update vite.config.ts

--- vite-react-main/vite.config.ts
+++ vite-react-main/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gate: resolve(__dirname, 'gate.html'),
+        success: resolve(__dirname, 'success.html'),
+        osakaTourViewer: resolve(__dirname, 'osaka-tour-viewer.html'),
+      },
+    },
+  },
 })
